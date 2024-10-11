07981b01e20163afee06492531f8e173c18c14fc
SimySlime <Anujsim2005@gmail.com>
2024-10-11 18:51:23 -0500

    submitted checkpoint #3

--- 11-strike_zone/solution.c
+++ 11-strike_zone/solution.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 void print_bar(int size, float progress, int strike_zone) {
     int i, start, end;
 
     start = round(size / 2.0 - strike_zone / 2.0);
-    end = round(size / 2.0 + strike_zone / 2.0);
+   end = round(size / 2.0 + strike_zone / 2.0);
 
     printf("start: %d, end: %d\n", start, end);
 
     printf("|");
     for (i = 0; i < size; i++) {
         if (i == start) {
-            printf("[");
+           printf("[");
         }
         if (i == end) {
             printf("]");
@@ -27,8 +28,18 @@ void print_bar(int size, float progress, int strike_zone) {
 }
 
 int main(int argc, char *argv[]) {
-    print_bar(10, 0.5, 1);
-    print_bar(10, 0.5, 2);
-    print_bar(15, 0.3, 4);
+    if (argc != 4) {
+        printf("Usage: %s size progress zone_width\n", argv[0]);
+        return 1;
+    }
+    int size = atoi (argv[1]);
+    float progress = atof(argv[2]);
+    int zone_width = atoi(argv[3]);
+    print_bar(size, progress, zone_width);
+
+    for (int i = 0; i < 100; i++){
+        float progres = (float) i / 100;
+        print_bar(100 , progres, 10);
+    }
     return 0;
 }
